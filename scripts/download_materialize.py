23ec10011ced0aae0d6203945f0f1295609af369
Henry Lindeman <hmlindeman@yahoo.com>
2025-04-19 23:00:11 -0700

    add notebook 0 (and accompanying download script) and incorporate feedback from eric (#27)
    Signed-off-by: Henry Lindeman <hmlindeman@yahoo.com>

--- scripts/download_materialize.py
+++ scripts/download_materialize.py
@@ -12,14 +12,17 @@ import tarfile
 
 def main() -> None:
     workshop_root = Path(__file__).parent.parent
-    materialize_dir = workshop_root / "materialize"
+    materialize_dir = workshop_root / "materialize2"
     materialize_dir.mkdir(exist_ok=True)
-    url = "https://aryn-public.s3.us-east-1.amazonaws.com/haystack_documents/alldocs-partitioned.tar.xz"
-
-    print(f"GET {url} and unpack it")
-    resp = requests.get(url, stream=True)
-    with tarfile.open(fileobj=resp.raw, mode='r|xz') as tf:
-        tf.extractall(materialize_dir)
+    urls = [
+        "https://aryn-public.s3.us-east-1.amazonaws.com/haystack_documents/alldocs-partitioned.tar.xz",
+        "https://aryn-public.s3.us-east-1.amazonaws.com/haystack_documents/about-to-ingest.tar.xz"
+    ]
+    for url in urls:
+        print(f"GET {url} and unpack it")
+        resp = requests.get(url, stream=True)
+        with tarfile.open(fileobj=resp.raw, mode='r|xz') as tf:
+            tf.extractall(materialize_dir)
 
 
 if __name__ == "__main__":
